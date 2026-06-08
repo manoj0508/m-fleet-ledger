829c3f44eb56c2006ef3dd785785710689b81215
manoj0508 <manoj.k0508@gmail.com>
2026-06-08 18:39:37 +0530

    m-fleet-ledger project:     basic structure is created

---
+++ src/main/java/com/fleet/ledger/app/MFleetLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.ledger.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MFleetLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MFleetLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleet/ledger/app/MFleetLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.ledger.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MFleetLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
